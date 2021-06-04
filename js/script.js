5fcf82ff1e2b65277fd299643972b49e0a369afe
Chan Li Liao <cliao119@gmail.com>
2021-06-04 18:10:44 -0400

    contact function is ready

--- js/script.js
+++ js/script.js
@@ -152,3 +152,38 @@ function portfolioItemDetails(portfolioItem) {
     '.portfolio-item-details'
   ).innerHTML;
 }
+
+// Contact form
+$(document).ready(function () {
+  $('.submit-btn').click(function (e) {
+    console.log('click');
+
+    let name = $('.name').val();
+    let email = $('.email').val();
+    let subject = $('.subject').val();
+    let message = $('.message').val();
+    let statusElm = $('.status');
+    statusElm.empty();
+
+    if (email.length > 5 && email.includes('@') && email.includes('.')) {
+      statusElm.append('<div>Email is valid</div>');
+    } else {
+      e.preventDefault();
+      statusElm.append('<div>Email is not valid</div>');
+    }
+
+    if (subject.length > 0) {
+      statusElm.append('<div>Subject is valid</div>');
+    } else {
+      e.preventDefault();
+      statusElm.append('<div>Subject is not valid</div>');
+    }
+
+    if (message.length > 0) {
+      statusElm.append('<div>Message is valid</div>');
+    } else {
+      e.preventDefault();
+      statusElm.append('<div>Message is not valid</div>');
+    }
+  });
+});
